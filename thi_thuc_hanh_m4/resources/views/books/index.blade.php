7082f5c984eb25acc2ac8a3762cb35b71d42ee19
nguyendinhphong123 <deepsea221297@gmail.com>
2023-03-16 13:50:29 +0700

    thi-thuc-hanh-m4

--- thi_thuc_hanh_m4/resources/views/books/index.blade.php
+++ thi_thuc_hanh_m4/resources/views/books/index.blade.php
@@ -1,4 +1,7 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <div class="container">
+    
     {{-- @if (session('status'))
     <div class="alert alert-success" role="alert">
         {{ session('status') }}
@@ -6,7 +9,22 @@
 @endif --}}
     <table border="1" class="table" style="text-align:center">
         <h1>Danh mục sách</h1>
-      
+        <div class="col-lg-6 col-md-5 col-sm-4 hidden-xs">
+            <!-- Search -->
+            <div class="search_box">
+                <div class="search_wrapper">
+                
+    
+                    <form  class="form-inline my-2 my-lg-0" action="{{ route('books.index') }}" method="GET">
+                        @csrf
+                        <button  style="float:right" class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+                          <input style="width: 300px; margin-right: 10px; float:right"  class="form-control" method="GET" name="book" type="text" placeholder="Tìm kiếm theo tên sản phẩm">
+          
+                          </form>
+                </div>
+            </div>
+            <!-- End Search -->
+        </div>
         <a href="{{route('books.create')}}" class="btn btn-info">Thêm mới</a>
         <thead>
             <tr>
@@ -47,20 +65,6 @@
         </table>
       
         {{ $books->links() }}
+        <a class="btn btn-primary" href="{{route('books.index')}}">Trang chủ</a>
     </div>
-    <div class="col-lg-6 col-md-5 col-sm-4 hidden-xs">
-        <!-- Search -->
-        <div class="search_box">
-            <div class="search_wrapper">
-            
-
-                <form  class="form-inline my-2 my-lg-0" action="{{ route('books.index') }}" method="GET">
-                    @csrf
-                    <button  style="float:right" class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
-                      <input style="width: 300px; margin-right: 10px; float:right"  class="form-control" method="GET" name="book" type="text" placeholder="Tìm kiếm theo tên sản phẩm">
-      <a href="{{route('books.index')}}">Trang chủ</a>
-                      </form>
-            </div>
-        </div>
-        <!-- End Search -->
-    </div>
+   
